b4967a8b46dd9c0ddab5251fa05bbe0dcd4bce18
Bartłomiej Kłuś <97251883+Onestyle7@users.noreply.github.com>
2025-01-25 12:33:31 +0100

    Update firebase.js

--- next-excercies/app/lib/firebase.js
+++ next-excercies/app/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSV7iv2AKl7-8Y5Xdkm90ezWXPl1b_-2g",
-  authDomain: "next-app-f25f9.firebaseapp.com",
-  projectId: "next-app-f25f9",
-  storageBucket: "next-app-f25f9.firebasestorage.app",
-  messagingSenderId: "1027849786535",
-  appId: "1:1027849786535:web:c8fc831301f49e2e677782",
-  measurementId: "G-7DD5H6RB9S"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
